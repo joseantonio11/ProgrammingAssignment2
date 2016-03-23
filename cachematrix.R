a53eda8e9f257236fb5d18855ea7eacfd454430e
Jose Antonio Ribeiro Neto <joseantonio@me.com>
2016-03-23 07:07:23 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## the following function creates
+## a special matrix 
+## called inverse matrix 
+## that caches its inverse
+## using as Script Model from the example "Caching the Mean of a Vector"
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mtx = matrix()) {
+  inversemat <- NULL
+  set <- function(x) {
+    mtx <<- x;
+    inversemat <<- NULL;
+  }
+  get <- function() return(mtx);
+  setinv <- function(inv) inversemat <<- inv;
+  getinv <- function() return(inversemat);
+  return(list(set = set, get = get, setinv = setinv, getinv = getinv))
 }
 
+## The function solve computes the inverse of one matrix 
+## The matrix have to be an invertible matrix
+## The cacheSolve functtion will retrieve it from the cache
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(mtx, ...) {
+  inversemat <- mtx$getinv()
+  if(!is.null(inversemat)) {
+    message("Getting cached data...")
+    return(inversemat)
+  }
+  data <- mtx$get()
+  inversemat <- solve(data, ...)
+  mtx$setinv(inversemat)
+  return(inversemat)
 }
+
+## try this test
+X <- matrix(rpois(36,6), nrow = 6)
+dX <- makeCacheMatrix(X)
+dX$get()
+cacheSolve(dX)
